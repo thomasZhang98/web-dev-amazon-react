cb4b1bf1daedcf5f41f3386911f1bf8c193fe97e
tingger2000 <yuting.ger@gmail.com>
2022-05-01 10:28:02 -0400

    update details

--- src/components/amazon/Details/index.js
+++ src/components/amazon/Details/index.js
@@ -35,17 +35,17 @@ const Details = () => {
     feature_bullets: [],
   });
 
-    const [ourProductDetails, setOurProductDetails] = useState({
-        asin: asin,
-        link: '',
-        image: 'not-found.png',
-        title: 'Loading...',
-        brand: '',
-        price: '',
-        feature_bullets: [],
-        bookmarks: [],
-        comments: []
-    })
+  const [ourProductDetails, setOurProductDetails] = useState({
+    asin: asin,
+    link: "",
+    image: "not-found.png",
+    title: "Loading...",
+    brand: "",
+    price: "",
+    feature_bullets: [],
+    bookmarks: [],
+    comments: [],
+  });
 
   const fetchProductByAsinFromAmazon = async () => {
     const response = await axios(`${product_url}=${asin}`);
@@ -75,40 +75,58 @@ const Details = () => {
     await fetchProductByAsinFromLocalAPI();
   };
 
-    const comment = () => {
-        return <div>
-            {productDetails.comments.map(c => {
-                return (
-                    <div className="list-group-item">
-                        <h5 className="fw-bold">{c.user}</h5>
-                        {c.comment}
-                    </div>
-                )
-            })}
-        </div>
-    }
-
   const comment = () => {
     return (
-        <div className="mt-2">
-            {productDetails === {} ? <div>Loading</div> :
-                <>
-                    <div className="d-flex align-items-start">
-                        <img src={productDetails.main_image.link} className="w-25" alt={"loading..."}/>
-                        <div className="float-start ms-3 w-100">
-                            <h4>{productDetails.title === 'Loading...' ? <i className="fas fa-spinner fa-pulse me-2"></i> : ''}{productDetails.title}</h4>
-                            <div>{productDetails.brand}</div>
-                            <h5>${productDetails.buybox_winner.price.value}</h5>
-                            <SecureContent>
-                                <div>
-                                    <button className="btn btn-success">Make Order</button>
-                                    <button className="btn btn-primary ms-2" onClick={handleBookmarks}>Bookmark
-                                        ({ourProductDetails.bookmarks.length})
-                                    </button>
-                                </div>
-                            </SecureContent>
-                        </div>
-                    </div>
+      <div>
+        {productDetails.comments.map((c) => {
+          return (
+            <div className="list-group-item">
+              <h5 className="fw-bold">{c.user}</h5>
+              {c.comment}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div className="mt-2">
+      {productDetails === {} ? (
+        <div>Loading</div>
+      ) : (
+        <>
+          <div className="d-flex align-items-start">
+            <img
+              src={productDetails.main_image.link}
+              className="w-25"
+              alt={"loading..."}
+            />
+            <div className="float-start ms-3 w-100">
+              <h4>
+                {productDetails.title === "Loading..." ? (
+                  <i className="fas fa-spinner fa-pulse me-2"></i>
+                ) : (
+                  ""
+                )}
+                {productDetails.title}
+              </h4>
+              <div>{productDetails.brand}</div>
+              <h5>${productDetails.buybox_winner.price.value}</h5>
+              <SecureContent>
+                <div>
+                  <button className="btn btn-success">Make Order</button>
+                  <button
+                    className="btn btn-primary ms-2"
+                    onClick={handleBookmarks}
+                  >
+                    Bookmark (
+                    {ourProductDetails && ourProductDetails.bookmarks.length})
+                  </button>
+                </div>
+              </SecureContent>
+            </div>
+          </div>
 
           <h5 className="mt-3">Highlights:</h5>
           <ul>
@@ -116,18 +134,21 @@ const Details = () => {
               <li>{point}</li>
             ))}
           </ul>
-                    <SecureContent>
-                        <div>
-                            <h5>Make a Comment:</h5>
-                            <textarea placeholder={"Put down your comment"} className="form-control"></textarea>
-                            <button className="btn btn-primary mt-2">Submit</button>
-                        </div>
-                    </SecureContent>
-                    <h5 className="mt-3">Comments:</h5>
-                </>
-            }
-        </div>
-    )
-}
+          <SecureContent>
+            <div>
+              <h5>Make a Comment:</h5>
+              <textarea
+                placeholder={"Put down your comment"}
+                className="form-control"
+              ></textarea>
+              <button className="btn btn-primary mt-2">Submit</button>
+            </div>
+          </SecureContent>
+          <h5 className="mt-3">Comments:</h5>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Details;
